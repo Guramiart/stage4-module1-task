0e02d45befb2106ffddb32e9953ecdc7fd3f5dae
Artem Pasko <guramibox777@gmail.com>
2023-07-20 12:20:43 +0300

    feat: add demo page

--- src/main/java/com/mjc/school/controller/DemoController.java
+++ src/main/java/com/mjc/school/controller/DemoController.java
@@ -1,4 +1,13 @@
 package com.mjc.school.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping("/")
+    public String demoPage() {
+        return "Demo Page";
+    }
 }
